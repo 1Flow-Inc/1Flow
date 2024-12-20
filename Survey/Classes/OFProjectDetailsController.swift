306c61c688bbb1cda9505fb406908c442372f7f1
Rohan Moradiya <rohan@tryskybox.com>
2024-12-20 18:22:47 +0530

    release 2024.12.20

--- Survey/Classes/OFProjectDetailsController.swift
+++ Survey/Classes/OFProjectDetailsController.swift
@@ -61,7 +61,7 @@ protocol ProjectDetailsManageable {
 final class OFProjectDetailsController: NSObject, ProjectDetailsManageable {
 
     static let shared = OFProjectDetailsController()
-    let oneFlowSDKVersion: String = "2024.10.01"
+    let oneFlowSDKVersion: String = "2024.12.20"
     var currentEnviromment: OneFlowEnvironment = .prod
     var currentLogLevel: OneFlowLogLevel = .none
 
